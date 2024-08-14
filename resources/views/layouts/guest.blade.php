d30275035727c1ed60fb35c25ba6378f52912dde
wildan1311 <wmaaf1311@gmail.com>
2024-08-14 10:47:12 +0700

    refactor: add toast sweetalert2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -16,10 +17,13 @@
     <link rel="stylesheet" href="{{ asset('template/css/global.css') }}">
     <link rel="stylesheet" href="{{ asset('template/css/index.css') }}">
     <script src="{{ asset('template/js/bootstrap.bundle.min.js') }}"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -29,7 +33,7 @@
 </head>
 
 <body>
-    @if($header)
+    @if ($header)
         @include('components.top')
         @include('components.navbar')
     @endif
@@ -43,20 +47,51 @@
 
 @stack('js')
 <script>
-    function deleteCart(id){
-                $.ajax({
-                    type: 'POST',
-                    url: '{{ route('cart.destroy') }}',
-                    data: {
-                        'product_id': id,
-                        '_token': '{{ csrf_token() }}'
-                    },
-                    success: function(data) {
-                        alert(data.message);
-                        window.location.reload()
-                    }
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        },
+        didDestroy: (toast) => {
+            window.location.reload();
+        }
+    })
+    window.addEventListener('load', function() {
+        @if (session('success'))
+            Toast.fire({
+                icon: 'success',
+                title: '{{ session('success') }}'
+            })
+        @endif
+        @if (session('error'))
+            Toast.fire({
+                icon: 'error',
+                title: '{{ session('error') }}'
+            })
+        @endif
+    })
+
+    function deleteCart(id) {
+        $.ajax({
+            type: 'POST',
+            url: '{{ route('cart.destroy') }}',
+            data: {
+                'product_id': id,
+                '_token': '{{ csrf_token() }}'
+            },
+            success: function(data) {
+                Toast.fire({
+                    icon: data.status,
+                    title: data.message
                 });
             }
+        });
+    }
 </script>
 
 </html>
