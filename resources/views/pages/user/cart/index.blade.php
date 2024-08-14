19a6c4e48d8d4b34e2d3187cf04e43b27acf015f
wildan1311 <wmaaf1311@gmail.com>
2024-08-14 07:20:13 +0700

    refactor: add image products show

--- resources/views/pages/user/cart/index.blade.php
+++ resources/views/pages/user/cart/index.blade.php
@@ -1,24 +1,8 @@
 <x-guest-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Shop') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <table class="table">
-                    <tr></tr>
-                </table>
-            </div>
-        </div>
-    </div> --}}
     <div class="container-xl">
         <div class="row about_h1 text-center mb-4">
             <div class="col-md-12">
-                <h4 class="col_blue">Cart</h4>
-                <h1 class="mt-3 mb-0">Best Seller Product</h1>
+                <h4 class="col_blue text-lg font-black">Cart</h4>
             </div>
         </div>
 
@@ -41,21 +25,21 @@
                             <div class="cart_3l1 mt-3 row ms-0 me-0">
                                 <div class="col-md-3 ps-0 col-3">
                                     <div class="cart_3l1i">
-                                        <a href="#"><img src="img/9.jpg" alt="abc" class="w-100"></a>
+                                        <a href="#"><img src="{{asset('storage/'. @$cart['image'])}}" alt="abc" class="w-100"></a>
                                     </div>
                                 </div>
                                 <div class="col-md-9 col-9">
                                     <div class="cart_3l1i1">
                                         <h6 class="fw-normal font_12 mt-3">{{$cart['name']}}</h6>
-                                        <h6 class="font_12 mt-3">Vendor</h6>
-                                        <h5 class="col_yell mt-3">Rp.{{$cart['price']}}</h5>
-                                        <h6 class="font_12 mt-3 mb-3">{{$cart['quantity']}}</h6>
+                                        <h6 class="font_12 mt-3 mb-4">Vendor</h6>
+                                        <h6 class="font_12 mt-3 mb-3 inline">{{$cart['quantity']}}x</h6>
+                                        <h5 class="col_yell mt-3 inline">Rp.{{number_format($cart['price'], 2, ',', '.')}}</h5>
                                     </div>
                                     <div class="cart_3l1i2">
-                                        <input type="number" min="1" value="{{$cart['quantity']}}" class="form-control inline"
+                                        <input type="number" min="1" value="{{$cart['quantity']}}" class="form-control inline w-52"
                                             placeholder="Qty" id="qty-{{$cart['id']}}">
-                                        <h6><button class="button_1" onclick="deleteCart({{$cart['id']}})">REMOVE</button></h6>
-                                        <h6><button class="button" onclick="addCart({{$cart['id']}})">UPDATE CART</button></h6>
+                                        <h6 class="inline"><button class="button_1" onclick="deleteCart({{$cart['id']}})">REMOVE</button></h6>
+                                        <h6 class="inline"><button class="button" onclick="addCart({{$cart['id']}})">UPDATE CART</button></h6>
                                     </div>
                                 </div>
                             </div>
@@ -64,9 +48,9 @@
                     <div class="col-md-4">
                         <div class="cart_3r">
                             <h6 class="head_1">SUBTOTAL</h6>
-                            <h5 class="text-center col_yell mt-3">{{collect($carts)->sum(function($cart){
+                            <h5 class="text-center col_yell mt-3">Rp.{{number_format(collect($carts)->sum(function($cart){
                                 return $cart['price'] * $cart['quantity'];
-                            })}}</h5>
+                            }), 2, ',', '.')}}</h5>
                             <hr>
                             <h6 class="text-center mt-3"><a class="button" href="{{route('cart.checkout')}}">PROCEED TO CHECKOUT</a>
                             </h6><br>
